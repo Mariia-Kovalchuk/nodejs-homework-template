328a105273dd2114bed8b80f2a233a4d90c31061
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-09-23 02:53:20 +0300

    REST API

--- model/index.js
+++ model/index.js
@@ -1,15 +1,110 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises')
+const path = require('path');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath)
+    const contactList = JSON.parse(contacts);
+    return contactList
+        
+  } catch (error) {
+    throw error
+  }
+};
 
-const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find(item => item.id === Number(contactId));
+    if (!contact) {
+      return null
+    }
+    return contact
+        
+  } catch (error) {
+    throw error
+  }
+
+};
+
+const updateContactsDb = async (newList) => {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(newList));
+  } catch (error) {
+    throw error
+  }
+};
+
+const checkPhoneUniqueness = async (phone) => {
+  const contacts = await listContacts();
+  const contact = contacts.find(contact => contact.phone === phone);
+  if (contact) {
+    return contact
+  } else {
+    return false
+  }
+};
+
+
+const removeContact = async (contactId) => {
+      try {
+        const contacts = await listContacts();
+        const idx = contacts.findIndex(({ id }) => id === Number(contactId));
+        if (idx === -1) {
+            return null;
+        }
+        const newContacts = contacts.filter(item => item.id !== Number(contactId));
+        await updateContactsDb(newContacts);
+        return true;
+        
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+const addContact = async (body) => {
+  try {
+    const { name, email, phone } = body;
+    const newContact = {
+      id: 0,
+      name,
+      email,
+      phone
+    };
+    const contacts = await listContacts();
+    const listLength = contacts.length;
+    newContact.id = contacts[listLength - 1].id + 1;
+    const newContacts = [...contacts, newContact];
+    await updateContactsDb(newContacts);
+    return newContact;
+        
+  } catch (error) {
+    throw error
+  };
+
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(({ id }) => id === Number(contactId));
+    if (idx===-1) {
+      return null
+    };
+    contacts[idx] = { ...contacts[idx], ...body }
+    await updateContactsDb(contacts);
+    return contacts[idx];
+
+  } catch (error) {
+    throw error
+  }
+  
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +112,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
+  checkPhoneUniqueness
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,77 @@
 const express = require('express')
 const router = express.Router()
+const contactsHendlers = require('../../model');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contactList = await contactsHendlers.listContacts();
+// console.log(contactList);
+  res.json({ contactList }).status(200)
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  // console.log(contactId);
+  const contact = await contactsHendlers.getContactById(contactId)
+  console.log(contact);
+  if (contact) {
+    res.status(200).json(contact)
+  } else {
+    res.status(404).json({ message: `Not found. ` })
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { body } = req;
+  const { name, email, phone } = body;
+  if (!name) {
+    return res.status(400).json({ message: `missing name field` });
+  }
+  if (!email) {
+    return res.status(400).json({ message: `missing email field` });
+  }
+  if (!phone) {
+   return  res.status(400).json({ message: `missing phone field` })
+  }
+  const contactWithSamePhone = await contactsHendlers.checkPhoneUniqueness(phone)
+  if (contactWithSamePhone) {
+    res.status(400).json({ message: `You already have contact ${contactWithSamePhone.name} with phone number ${phone}. ` })
+  } else {
+    res.status(201).json(await contactsHendlers.addContact(body))
+      
+  }
+
+
+
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const isContactDeleted = await contactsHendlers.removeContact(contactId);
+  if (isContactDeleted) {
+    res.status(200).json({ message: "contact deleted" })
+  } else {
+    res.status(404).json({ message: "Not found" })
+    
+  }
 })
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { contactId } = req.params;
+  const { body } = req;
+  if (Object.keys(body).length===0) {
+    return res.status(400).json({ message: "missing fields" })
+  }
+  const contactWithSamePhone = await contactsHendlers.checkPhoneUniqueness(body.phone)
+  if (contactWithSamePhone) {
+   return res.status(400).json({ message: `You already have contact ${contactWithSamePhone.name} with phone number ${body.phone}. ` })
+  };
+  const updatedContact = await contactsHendlers.updateContact(contactId, body)
+  if (updatedContact) {
+    res.status(200).json(updatedContact)
+  } else {
+    res.status(404).json({ message: `Id ${contactId} Not found` })
+  }
+    
+});
 
 module.exports = router
